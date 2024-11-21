7bb21b946204390e101ee3f5407ccc01bf715cff
Andrew <119732698+andrewnms@users.noreply.github.com>
2024-11-21 20:48:54 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -167,9 +167,10 @@ const Index = () => {
     <div className="mt-8 text-xs text-gray-500 stagger">
       © {new Date().getFullYear()} SeriousMisfits. All rights reserved.
     </div>
-);
-};
   </div>
 </footer>
+);
+};
+
 
 export default Index;
