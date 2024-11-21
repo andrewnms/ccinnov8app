c670157e6c4165490b02772ae925ba0fe54cefff
Andrew <119732698+andrewnms@users.noreply.github.com>
2024-11-21 21:29:12 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -55,7 +55,12 @@ const Index = () => {
           <p className="text-xl md:text-2xl mb-12 font-mono text-gray-400 stagger">
             experience a new way of learning entrepreneurship with the all new ccinnov8 class.
           </p>
-          <p className="text-lg mb-8 text-gray-300 stagger">misfits are welcomed.</p>
+          <p className="text-lg mb-8 text-gray-300 stagger">
+  misfits are welcomed.
+</p>
+<p className="text-sm text-gray-400 stagger">
+  (all students from all colleges)
+</p>
           <div className="flex flex-col gap-4 justify-center stagger">
   <Button
     onClick={handleWaitlist}
